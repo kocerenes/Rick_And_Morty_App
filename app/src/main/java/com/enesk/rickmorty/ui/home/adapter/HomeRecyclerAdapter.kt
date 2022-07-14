3e1dd6ece497d9f4362c4b1cb62d2408f89afcfc
kocerenes <eneskocerr41@gmail.com>
2022-07-14 15:15:09 +0300

    Add enum class for character status.

--- app/src/main/java/com/enesk/rickmorty/ui/home/adapter/HomeRecyclerAdapter.kt
+++ app/src/main/java/com/enesk/rickmorty/ui/home/adapter/HomeRecyclerAdapter.kt
@@ -1,5 +1,7 @@
 package com.enesk.rickmorty.ui.home.adapter
 
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import coil.load
 import com.enesk.rickmorty.R
 import com.enesk.rickmorty.data.remote.model.Character
 import com.enesk.rickmorty.databinding.ItemCharactersBinding
+import com.enesk.rickmorty.utils.CharacterStatusEnums
 import javax.inject.Inject
 
 class HomeRecyclerAdapter():RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHolder>(){
@@ -39,6 +42,8 @@ class HomeRecyclerAdapter():RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHol
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        val character = diffList[position]
+
         holder.binding.apply {
             tvCharacterName.text = diffList[position].name
             tvCharacterGender.text = diffList[position].gender
@@ -47,6 +52,19 @@ class HomeRecyclerAdapter():RecyclerView.Adapter<HomeRecyclerAdapter.HomeViewHol
             ivCharacterProfile.load(imgCharacterProfile){
                 crossfade(true)
                 crossfade(100)
+            }
+
+            when(character.status){
+                CharacterStatusEnums.CHARACTER_ALIVE.value -> imgCharacterStatus.setColorFilter(
+                    Color.parseColor("#14D91B")
+                )
+                CharacterStatusEnums.CHARACTER_DEAD.value -> imgCharacterStatus.setColorFilter(
+                    Color.parseColor("#FF0800")
+                )
+                CharacterStatusEnums.CHARACTER_UNKNOWN.value -> imgCharacterStatus.setColorFilter(
+                    Color.parseColor("#E3E3E3")
+                )
+                else -> imgCharacterStatus.setColorFilter(Color.parseColor("#F8F816"))
             }
 
         }
---
+++ app/src/main/java/com/enesk/rickmorty/utils/CharacterStatusEnums.kt
@@ -0,0 +1,9 @@
+package com.enesk.rickmorty.utils
+
+enum class CharacterStatusEnums(val value: String) {
+
+    CHARACTER_ALIVE("Alive"),
+    CHARACTER_DEAD("Dead"),
+    CHARACTER_UNKNOWN("unknown")
+
+}
