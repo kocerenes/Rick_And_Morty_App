3a6a3b623dd7cf5af3c02a63a77ac9059768a09c
kocerenes <eneskocerr41@gmail.com>
2022-07-24 12:44:23 +0300

    Create bottom sheet view design and query model

---
+++ app/src/main/java/com/enesk/rickmorty/data/local/model/CharacterQueryModel.kt
@@ -0,0 +1,7 @@
+package com.enesk.rickmorty.data.local.model
+
+data class CharacterQueryModel(
+    var name : String? = null,
+    var status : String? = null,
+    var gender : String? = null
+)
---
+++ app/src/main/java/com/enesk/rickmorty/ui/bottom_sheet/FilterBottomSheetFragment.kt
@@ -0,0 +1,10 @@
+package com.enesk.rickmorty.ui.bottom_sheet
+
+import com.enesk.rickmorty.data.local.model.CharacterQueryModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class FilterBottomSheetFragment : BottomSheetDialogFragment() {
+
+
+
+}
