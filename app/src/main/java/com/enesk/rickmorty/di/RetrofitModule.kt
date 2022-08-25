be22b0f63a6081d1d37b93a8b60359a83d10f1fa
kocerenes <eneskocerr41@gmail.com>
2022-08-25 17:54:18 +0300

    Create Okhttp

--- app/src/main/java/com/enesk/rickmorty/di/RetrofitModule.kt
+++ app/src/main/java/com/enesk/rickmorty/di/RetrofitModule.kt
@@ -1,18 +1,44 @@
 package com.enesk.rickmorty.di
 
+import com.enesk.rickmorty.BuildConfig
 import com.enesk.rickmorty.data.remote.ApiFactory
 import com.enesk.rickmorty.utils.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggerInterceptor(): HttpLoggingInterceptor {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.NONE
+        }
+        return httpLoggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
 
     @Singleton
     @Provides
@@ -23,10 +49,12 @@ object RetrofitModule {
     @Singleton
     @Provides
     fun provideRetrofitAPI(
+        okHttpClient: OkHttpClient,
         gsonConverterFactory: GsonConverterFactory
     ): Retrofit{
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(gsonConverterFactory)
             .build()
     }
